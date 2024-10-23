58520a32adf8dac19d83e115012372f7a892eacb
vincent <vincentcharlebois@gmail.com>
2024-10-23 09:05:24 -0400

    Refactor ProposalsTable table styles

--- dashboard/src/lib/ProposalsTable.svelte
+++ dashboard/src/lib/ProposalsTable.svelte
@@ -13,14 +13,12 @@
   <h2>
     Proposals
   </h2>
-  <table class="w-full border-separate border-spacing-0 rounded-tl-lg">
-    <thead class="theader">
+  <table class="w-full border-separate border-spacing-0 rounded-tl-lg mt-4">
+    <thead>
       <tr>
-        <div>
-        <th class="rounded-tl-lg px-4 py-2">ID</th>  <!-- Top-left corner rounded -->
-        <th class="px-4 py-2">Title</th>
-        <th class="rounded-tr-lg px-4 py-2">Status</th> <!-- Top-right corner rounded -->
-      </div>
+          <th class="rounded-tl-lg rounded-bl-lg px-4 py-2 thead">ID</th> 
+          <th class="px-4 py-2 thead">Title</th>
+          <th class="rounded-tr-lg rounded-br-lg px-4 py-2 thead">Status</th>
       </tr>
     </thead>
     <tbody>
@@ -36,7 +34,7 @@
 </div>
 
 <style>
-  .theader {
+  .thead {
     background-color: #D9D9D9;
     color: #000;
   }
