898e9581c36ee0b16b36d0c81d55dd586b583836
vincent <vincentcharlebois@gmail.com>
2024-10-23 17:33:26 -0400

    Refactor App.svelte to include CSV loading and decision log components

--- dashboard/src/App.svelte
+++ dashboard/src/App.svelte
@@ -2,11 +2,15 @@
   import ParticipationGraph from './lib/ParticipationGraph.svelte';
   import VotesBarGraph from './lib/VotesBarGraph.svelte';
   import ProposalsTable from './lib/ProposalsTable.svelte';
+  import LoadDecisions from './lib/LoadDecisions.svelte';
+  import DecisionLog from './lib/DecisionLog.svelte';
+  import { csvLoaded } from './stores';
   
+  $: console.log(csvLoaded);
 </script>
 
 <main>
-  <h1 class="text-left mb-6 mt-2 text-center md:text-left mb-12 md:mb-6 lg:mb-2">
+  <h1 class="text-left mb-6 mt-2 text-center md:text-left mb-12 md:mb-6 lg:mb-8">
     Governance Dashboard
   </h1>
   <div class="flex flex-col lg:flex-row lg:space-x-8">
@@ -15,7 +19,13 @@
   </div>
   
   <ProposalsTable />
-  
+
+  {#if !$csvLoaded}
+    <LoadDecisions />
+  {:else}
+    <DecisionLog />
+  {/if}
+
 </main>
 
 <style>
