5a330a599e49690089afeb831487089fd94c5607
vincent <vincentcharlebois@gmail.com>
2024-10-28 21:26:36 -0400

    Update participant count styling

--- dashboard/src/lib/TotalVoters.svelte
+++ dashboard/src/lib/TotalVoters.svelte
@@ -9,7 +9,7 @@
     });
   </script>
   
-  <div class="w-full flex flex-col component bg-gray-100 rounded-lg p-6 shadow-lg">
+  <div class="w-full flex flex-col component rounded-lg p-6 shadow-lg">
     <h2 class="font-semibold text-left">Participants</h2>
     <div class="flex flex-row justify-center items-center h-full">
         <p class="text-5xl font-bold my-4 text-left">
@@ -23,7 +23,7 @@
 
 <style>
     div p span {
-        color: white;
+        color: #000;
         text-decoration: underline;
         text-decoration-color: #9900FC;
         text-decoration-thickness: 5px;
