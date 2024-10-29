e6803f6938b5ffaebe55b7e6258b43025f456c21
vincent <vincentcharlebois@gmail.com>
2024-10-28 21:27:50 -0400

    update label and styling for decision log file upload

--- dashboard/src/lib/LoadDecisions.svelte
+++ dashboard/src/lib/LoadDecisions.svelte
@@ -9,8 +9,10 @@
     };
   </script>
   
-  <div class="file-upload my-4 flex flex-col border-2 border-purple rounded p-2">
-    <h2 class="text-left">Load CSV for Decision Log</h2>
-    <br>
+  <div class="file-upload my-4 flex flex-col border-2 border-purple rounded p-2 items-baseline">
+    <h2 class="text-left">Decision Log</h2>
+    <label>
+    <p class="text-left purple">Load CSV</p>
     <input type="file" accept=".csv" on:change={handleFileUpload} />
+  </label>
   </div>
