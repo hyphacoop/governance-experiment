4a88c5ae7a1e8419e416d3f444ba025250fbc972
vincent <vincentcharlebois@gmail.com>
2024-10-28 20:12:35 -0400

    Refactor TotalVoters component: Add participant count and styling

---
+++ dashboard/src/lib/TotalVoters.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { votedCount } from "../stores";
+  
+    let totalVoters;
+  
+    // Subscribe to the `votedCount` store to get the total voter count
+    $: votedCount.subscribe(value => {
+      totalVoters = value;
+    });
+  </script>
+  
+  <div class="w-full flex flex-col component bg-gray-100 rounded-lg p-6 shadow-lg">
+    <h2 class="font-semibold text-left">Participants</h2>
+    <div class="flex flex-row justify-center items-center h-full">
+        <p class="text-5xl font-bold my-4 text-left">
+            <span>
+                {totalVoters}
+                </span>
+                 / 12
+        </p>
+      </div>
+</div>
+
+<style>
+    div p span {
+        color: white;
+        text-decoration: underline;
+        text-decoration-color: #9900FC;
+        text-decoration-thickness: 5px;
+    }
+</style>
