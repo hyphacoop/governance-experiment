9dbd494ce1deff25be24a87e5ad08e35d2252477
vincent <vincentcharlebois@gmail.com>
2024-12-06 18:28:38 -0500

    Add dotenv support to Vite configuration for environment variable management

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/governance-experiment/',
   plugins: [svelte()],
+  define: {
+    'process.env': process.env,
+  },
 })
