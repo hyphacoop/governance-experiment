8ba723a20cb5d16feecea86a15a4f1937e81b1e1
vincent <vincentcharlebois@gmail.com>
2024-12-06 18:27:50 -0500

    Add Google authentication component to LoadDecisions

--- dashboard/src/lib/LoadDecisions.svelte
+++ dashboard/src/lib/LoadDecisions.svelte
@@ -1,4 +1,6 @@
 <script>
+    import GoogleAuth from './GoogleAuth.svelte';
+
     import { loadDecisionLog } from '../stores';
   
     const handleFileUpload = async (event) => {
@@ -11,8 +13,12 @@
   
   <div class="file-upload my-4 flex flex-col border-2 border-purple rounded p-2 items-baseline">
     <h2 class="text-left">Decision Log</h2>
-    <label>
-    <p class="text-left purple">Load CSV</p>
-    <input type="file" accept=".csv" on:change={handleFileUpload} />
-  </label>
+    <div class="flex flex-col items-start">
+      <GoogleAuth />
+      <label>
+        <p class="text-left purple">Or load CSV</p>
+        <input type="file" accept=".csv" on:change={handleFileUpload} />
+      </label>
+    </div>
+    
   </div>
