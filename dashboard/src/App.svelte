1b5656c316dcc9b9a0124c9c1ec76c5a1342bc86
vincent <vincentcharlebois@gmail.com>
2024-12-09 15:27:04 -0500

    Refactor App.svelte to improve code readability and update conditional rendering logic for decision log and vote data

--- dashboard/src/App.svelte
+++ dashboard/src/App.svelte
@@ -1,23 +1,22 @@
 <script>
   // currently not using the LineGraph
-  import LineGraph from './lib/LineGraph.svelte';
+  //import LineGraph from "./lib/LineGraph.svelte";
 
-  import TotalVoters from './lib/TotalVoters.svelte';
-  import BarGraph from './lib/BarGraph.svelte';
-  import ProposalsTable from './lib/ProposalsTable.svelte';
-  import LoadDecisions from './lib/LoadDecisions.svelte';
-  import DecisionLog from './lib/DecisionLog.svelte';
+  import TotalVoters from "./lib/TotalVoters.svelte";
+  import BarGraph from "./lib/BarGraph.svelte";
+  import ProposalsTable from "./lib/ProposalsTable.svelte";
+  import LoadDecisions from "./lib/LoadDecisions.svelte";
+  import DecisionLog from "./lib/DecisionLog.svelte";
 
-  import { isDecisionLogLoaded, decisionLogData, loadVoteData, clearVoteData, voteDataLoaded } from './stores';
+  import { isDecisionLogLoaded, loadVoteData, voteDataLoaded } from "./stores";
 
   // Track loaded state for vote data
   let isVoteDataLoaded;
-  $: voteDataLoaded.subscribe(value => isVoteDataLoaded = value);
-
+  $: voteDataLoaded.subscribe((value) => (isVoteDataLoaded = value));
 
   // Track loaded state for decision log
   let decisionLogLoadState;
-  $: isDecisionLogLoaded.subscribe(value => decisionLogLoadState = value);
+  $: isDecisionLogLoaded.subscribe((value) => (decisionLogLoadState = value));
 
   // Handler for vote data file upload
   const handleVoteDataUpload = async (event) => {
@@ -29,32 +28,24 @@
 <main>
   <h1>Governance Dashboard</h1>
 
-  <!-- Display Vote Data Graphs and Table if vote data is loaded -->
+  <!-- Display or Load decision log data -->
   {#if decisionLogLoadState}
     <DecisionLog />
-   
+    <!-- Display Vote Data Graphs and Table if vote data is loaded -->
+    {#if isVoteDataLoaded}
+      <div class="flex flex-col lg:flex-row lg:space-x-7">
+        <BarGraph />
+        <!-- Hiding LineGraph until it serves a purpose other than showing results -->
+        <!-- 
+        <LineGraph />
+      -->
+        <TotalVoters />
+      </div>
+      <ProposalsTable />
+    {/if}
   {:else}
-
     <LoadDecisions />
-  <!--
-
-     <div class="flex flex-col lg:flex-row lg:space-x-7">
-      <BarGraph />
-      <TotalVoters />
-    </div>
-    <ProposalsTable />
-    <div class="my-4 flex flex-col border-2 border-purple rounded p-2 items-baseline">
-      <h2 class="text-left">Vote Results</h2>
-      <br>
-      <label>
-        <p class="text-left purple">Load CSV</p>
-        <input type="file" accept=".csv" on:change={handleVoteDataUpload} />
-      </label>
-    </div>
-    -->
   {/if}
-
-
 </main>
 
 <style>
