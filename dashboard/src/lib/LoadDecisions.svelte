540b176bd0587835873cc08d377b3c727bfcecae
vincent <vincentcharlebois@gmail.com>
2025-01-22 23:44:57 -0500

    Enhance LoadDecisions component layout

--- dashboard/src/lib/LoadDecisions.svelte
+++ dashboard/src/lib/LoadDecisions.svelte
@@ -12,13 +12,22 @@
   </script>
   
   <div class="file-upload my-4 flex flex-col border-2 border-purple rounded p-2 items-baseline">
-    <h2 class="text-left">Decision Log</h2>
-    <div class="flex flex-col items-start">
+    <h2 class="text-left ml-12 mt-6">Decision Log</h2>
+    <div class="flex flex-col items-start ml-12">
       <GoogleAuth />
-      <label class='flex flex-row'>
-        <p class="text-left purple mr-2">Or load CSV</p>
+      <label class='flex flex-col'>
+        <p class="text-left purple mr-2">
+          Or load CSV from 
+          <a class="purple underline" href="ttps://docs.google.com/spreadsheets/d/16vZwzK3OQmWWtRBtUAlk-ly435fvdj0BqOhLSesHkrY/edit?usp=sharing">
+            template
+          </a>
+        </p>
         <input type="file" accept=".csv" on:change={handleFileUpload} />
       </label>
     </div>
-    
   </div>
+  <style>
+    label {
+      margin-bottom: 1.5rem;
+    }
+  </style>
