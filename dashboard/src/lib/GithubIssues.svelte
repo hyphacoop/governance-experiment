d4db4b0a3028d429858ce7e8cfbbc91b8e55fe5e
vincent <vincentcharlebois@gmail.com>
2025-01-22 23:46:04 -0500

    Update GithubIssues component to conditionally apply class

--- dashboard/src/lib/GithubIssues.svelte
+++ dashboard/src/lib/GithubIssues.svelte
@@ -1,15 +1,22 @@
 <script>
   import { githubIssues } from "../stores";
+
+    // Reactive statement to update class based on githubIssues length
+    $: topDivClass = $githubIssues.length > 0 
+      ? "my-4 flex flex-col border-2 border-purple rounded p-2 items-baseline component" 
+      : "my-4 flex flex-col border-2 border-purple rounded p-2 items-baseline";
+
 </script>
 
-<div>
+<div class={topDivClass}>
+  <h2>Provide Feedback</h2>
   <blockquote class='my-4 p-4 pl-0 purple max-w-8xl text-left'>Below are the issues currently marked with a status of <span class='underline'>Feedback Required</span> on the project board.</blockquote>
   {#if $githubIssues.length > 0}
     <ul class="flex flex-row flex-wrap max-w-8xl">
       {#each $githubIssues as issue}
         <li class="border border-purple rounded-lg my-2 p-2">
           <a href="{issue.content.url}" target="_blank" rel="noopener noreferrer">
-            <strong>{issue.content.title}</strong>
+            <strong>{issue.content.title} 🔗</strong>
           </a>
         </li>
       {/each}
@@ -45,4 +52,9 @@
   li:hover a {
     color: white;
   }
+
+  .component {
+      border-style: dotted;
+      border: 2px dotted #000;
+    }
 </style>
