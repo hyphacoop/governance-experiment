70192c889bea52d4f9ba3327ce91dbc2b34a5f99
vincent <vincentcharlebois@gmail.com>
2025-01-27 11:21:16 -0500

    Create Export Data component

---
+++ dashboard/src/lib/ExportData.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { exportData } from '../stores.js';
+  </script>
+  
+<div class="flex flex-col items-start">
+  <button on:click={exportData} class="rounded">
+    Export Data
+  </button>
+</div>
+  <style>
+    button {
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    border: 1px solid #000;
+    background-color: #f5f5f5;
+    margin: 2rem 0;
+  }
+
+  button:hover {
+    background-color: black;
+    color: white;
+  }
+  </style>
